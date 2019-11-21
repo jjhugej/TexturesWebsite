ab37bb842b71f7081300b93677d0db3709ee6b5d
Jonathan <jonlillywebdev@gmail.com>
2019-11-21 18:43:11 -0500

    added more functionality to navbar

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log("ready!");
+  $(function() {
+    $(".navLiItem").hover(
+      function() {
+        $("#bookNowNavItem").css("background-color", "white");
+        $("#bookNowNavItem a").css("color", "black");
+        $("#bookNowNavItem, a").css("transition", "all .5s ease");
+      },
+      function() {
+        // on mouseout, reset the background colour
+        $("#bookNowNavItem").css("background-color", "#ba8f6a");
+        $("#bookNowNavItem a").css("color", "");
+      }
+    );
+  });
 });
